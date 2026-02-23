64804a7d09ca08713790b4c019b75093b9b399c4
Daksh <mrgear111@gmail.com>
2026-02-24 00:52:46 +0530

    backend fix

--- backend/main.py
+++ backend/main.py
@@ -95,7 +95,7 @@ app.add_middleware(
         "http://localhost:5173",    
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
-        "https://specter-scan-7opa.vercel.app",
+        "https://specter-scan.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],           
